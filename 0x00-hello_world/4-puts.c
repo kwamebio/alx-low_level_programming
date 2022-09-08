d00dc857b1eef96a46c8445cd9b5ac676e6ba952
Kwame Bio Agyemang Duah <kwameagyemang73@gmail.com>
2022-09-08 15:54:03 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * Description: main - Prints out a string to stdout.
  * Return: 0 if success.
- * /
+ */
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+			puts("\"Programming is like building a multilingual puzzle");
 
-					return (0);
+						return (0);
 
-					}
+}
