a287b040175455e5a6222bf0aa91fa887dbd3736
kwamebio <104377977+kwamebio@users.noreply.github.com>
2022-09-10 01:06:43 +0000

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,17 +3,16 @@
  * main - prints the alphabet in reverse.
 (*
  * Return: 0 on success
- *
  */
 int main(void)
 {
-		char c = 'z';
+	char c = 'z';
 
-			while (c >= 'a')
-					{
-								putchar(c);
-										c--;
-											}
-				putchar('\n');
-					return (0);
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
 }
