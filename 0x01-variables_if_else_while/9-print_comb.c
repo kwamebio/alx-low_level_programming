e8ff848845552091de686917c9419455edaafaa4
kwamebio <104377977+kwamebio@users.noreply.github.com>
2022-09-10 01:08:09 +0000

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,19 +6,19 @@
  */
 int main(void)
 {
-		int i = '0';
+	int i = '0';
 
-			while (i <= '9')
-					{
-								putchar(i);
-										if (i != '9')
-													{
-																	putchar(',');
-																				putchar(' ');
-																						}
-												++i;
-													}
-				putchar('\n');
-					return (0);
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		++i;
+	}
+	putchar('\n');
+	return (0);
 
 }
